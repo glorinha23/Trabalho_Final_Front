fe920cd06a7033ad98106c8865f74102c336dc7a
Glória Oliveira <130005832+glorinha23@users.noreply.github.com>
2023-11-23 20:08:09 -0300

    Add files via upload

---
+++ src/data/categories.js
@@ -0,0 +1,7 @@
+export const categories = {
+  food: { title: "Alimentação", color: "#0000FF", expense: true },
+  rent: { title: "Aluguel", color: "#8A2BE2", expense: true },
+  salary: { title: "Salário", color: "#008000", expense: false },
+  water: { title: "Água", color: "red", expense: true },
+  energy: { title: "Energia", color: "#FFA500", expense: true },
+};
